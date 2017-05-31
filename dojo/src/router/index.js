0f1fefd7b26cf444145b7dfc3e1f7204e09f3053
ccrespo-ciandt <carlos.hcrespo@gmail.com>
2017-05-31 11:31:01 -0300

    exericio 3

--- dojo/src/router/index.js
+++ dojo/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Exercicio2 from '@/components/Exercicio2'
+import Exercicio3 from '@/components/Exercicio3'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/Exercicio2',
       name: 'Exercicio2',
       component: Exercicio2
+    },
+    {
+      path: '/Exercicio3',
+      name: 'Exercicio3',
+      component: Exercicio3
     }
   ]
 })
